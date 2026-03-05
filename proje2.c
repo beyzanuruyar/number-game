3a96c7a2e6c7b895f74ba61e45ef700ac92210a0
beyzanur uyar <uyarbeyzanur690@gmail.com>
2026-03-05 10:42:35 +0300

    init

---
+++ proje2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int guess;
+    
+    srand(time(NULL));
+
+    int number = rand() % 1000 + 1;
+
+    do
+    {
+        printf("I chose a number between 1 and 1000, guess what it is?\n");
+        scanf("%d", &guess);
+        if(number<guess){
+            printf("number is smaller than your choose\n");
+        }
+
+        else if(number>guess){
+            printf("number is bigger than your choose\n");
+        }
+        if (number==guess)
+        {
+            printf("Congratulations! You guessed correctly.\n");
+        }
+        
+    } while (guess!=number);
+    
+     return 0;
+
+}
